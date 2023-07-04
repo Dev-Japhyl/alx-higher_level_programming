9219ecfaa8f3cc7d7dee7862b0b594bc7d9481ea
Dev-Japhyl <jegedejaphet123@gmail.com>
2023-07-04 16:28:10 -0700

    Test-Driven dev tasks

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is the "3-say_my-name" module.
+The 3-say_my_name  module supplies one function, say_my_name.
+"""
+
+def say_my_name(first_name, last_name=""):
+    """
+    @first_name = str with firstname value
+    @last_name = str with last_name value
+    """
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    print("My name is {:s} {:s}".format(first_name, last_name))
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This is the "5-test_indentation" module.
+The 5-text_indentation module supplies one function, text_indentation(text).
+"""
+
+
+def text_indentation(text):
+    """
+    @text = must be a string, otherwise raise a TypeError exception with the
+    message text must be a string
+    """
+    if isinstance(text, (str)) is False or text is None:
+        raise TypeError("text must be a string")
+    l = len(text) - 1
+    buffer = []
+    for i, char in enumerate(text):
+        buffer.append(char)
+        if char == ':' or char == '.' or char == '?':
+            print("".join(buffer).strip())
+            print()
+            buffer = []
+        elif i == l:
+            print("".join(buffer).strip(), end="")
