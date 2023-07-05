a70c1759c041b72b60379218d5113477f506a647
Dev-Japhyl <jegedejaphet123@gmail.com>
2023-07-05 16:54:46 -0700

    Test-Driven dev tasks

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,25 +1,13 @@
 #!/usr/bin/python3
-"""
-text_indentation - a function that prints a text with 2 new lines after each of
-these characters: ., ? and :
-Returns characters printed
-"""
-
-
 def text_indentation(text):
-    """
-    @text = must be a string, otherwise raise a TypeError exception with the
-    message text must be a string
-    """
-    if isinstance(text, (str)) is False or text is None:
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
-    d = len(text) - 1
-    buffer = []
-    for i, char in enumerate(text):
-        buffer.append(char)
-        if char == ':' or char == '.' or char == '?':
-            print("".join(buffer).strip())
-            print()
-            buffer = []
-        elif i == l:
-            print("".join(buffer).strip(), end="")
+    beg = 0
+    for idx, val in enumerate(text):
+        if val in '?:.':
+            print(text[beg:idx + 1].strip() + '\n')
+            beg = idx + 1
+    if not beg:
+        print(text, end='')
+    elif beg is not len(text):
+        print(text[beg:idx + 1].strip(), end='')
