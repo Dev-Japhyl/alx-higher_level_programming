8f915f16affc628c8b80a312b50e79c0636d2787
Dev-Japhyl <jegedejaphet123@gmail.com>
2023-07-17 18:05:29 -0700

    models & tests commit

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,81 +1,61 @@
 #!/usr/bin/python3
-"""Defines square class"""
+"""
+10. And now, the Square!
+"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square"""
-
+    """Write the class Square that inherits from Rectangle
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Initialize a new square
-        Args:
-            size (int): The size of the new square.
-            x (int): The X coordinate of the new square.
-            y (int): The y coordinate of the new square.
-            id (int): The identity of the new square.
-        """
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """The overloading __str__ method should return [Square] (<id>) <x>/<y>
+           - <size> - in our case, width or height
+        """
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
+            self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        """get/set the size of the square."""
+        """Size getter
+        """
         return self.width
 
     @size.setter
-    def size(self, value):
-        self.width = value
-        self.height = value
+    def size(self, size):
+        """Size setter
+        """
+        self.width = size
+        self.height = size
 
     def update(self, *args, **kwargs):
-        """Update the Square.
-        Args:
-            *args (ints): New attribute values.
-                - 1st argument represents id attribute
-                - 2nd argument represents size attribute
-                - 3rd argument represents x attribute
-                - 4th argument represents y attribute
-            **kwargs (dict): New key/value pairs of attributes.
+        """adding the public method def update(self, *args, **kwargs) that
+        assigns attributes: id, size, x, y.
         """
-        if args and len(args) != 0:
-            count = 0
-            for arg in args:
-                if count == 0:
-                    if arg is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif count == 1:
-                    self.size = arg
-                elif count == 2:
-                    self.x = arg
-                elif count == 3:
-                    self.y = arg
-                count += 1
-
-        elif kwargs and len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "id":
-                    if v is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = v
-                elif k == "size":
-                    self.size = v
-                elif k == "x":
-                    self.x = v
-                elif k == "y":
-                    self.y = v
+        if args:
+            attrs = ["id", "size", "x", "y"]
+            for i, e in enumerate(args):
+                setattr(self, attrs[i], e)
+            return
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
 
     def to_dictionary(self):
-        """Return dictionary representation of the Square"""
-        return {
-            "id": self.id,
-            "size": self.width,
-            "x": self.x,
-            "y": self.y
-        }
-
-    def __str__(self):
-        """Return the print() and str() representation of the square"""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        """Adding the public method def to_dictionary(self): that returns the
+        dictionary representation of a Square: id, size, x, y
+        """
+        dict = {}
+        for key, value in vars(self).items():
+            if key.startswith("_"):
+                if not key.endswith("width") and not key.endswith("height"):
+                    idx = key.index("__")
+                    dict[key[idx + 2:]] = value
+                else:
+                    dict["size"] = value
+            else:
+                dict[key] = value
+        return dict
